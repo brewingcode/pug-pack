ff6c3a19a0d3ad4ffe1efaf59a300618aac0b027
Alex <alex@brewingcode.net>
2017-10-08 19:45:47 -0700

    Change browserAction icon per-tab

--- css-grep/background.js
+++ css-grep/background.js
@@ -1,13 +1,24 @@
-var showing = false;
+var tabs = {};
+
+chrome.tabs.onActivated.addListener(function(obj) {
+  var id = obj.tabId;
+  if (!tabs[id]) {
+    tabs[id] = {
+      showing: false
+    };
+  }
+  chrome.browserAction.setIcon({ path: tabs[id].showing ? "active.png" : "inactive.png" });
+});
+
 chrome.browserAction.onClicked.addListener(function(tab) {
-  if (showing) {
+  if (tabs[tab.id].showing) {
     chrome.browserAction.setIcon({ path:"inactive.png" });
     chrome.tabs.executeScript(tab.id, { code: 'hide()' });
-    showing = false;
+    tabs[tab.id].showing = false;
   }
   else {
     chrome.browserAction.setIcon({ path:"active.png" });
     chrome.tabs.executeScript(tab.id, { file: 'grep.js' });
-    showing = true;
+    tabs[tab.id].showing = true;
   }
 });
