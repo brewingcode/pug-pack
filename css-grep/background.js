76841a75bd6e1b31d3d4497ea5107498bad9b13e
Alex <alex@brewingcode.net>
2017-10-08 18:53:17 -0700

    Toggle browserAction icon between active and inactive states

--- css-grep/background.js
+++ css-grep/background.js
@@ -1,10 +1,12 @@
 var showing = false;
 chrome.browserAction.onClicked.addListener(function(tab) {
   if (showing) {
+    chrome.browserAction.setIcon({ path:"inactive.png" });
     chrome.tabs.executeScript(tab.id, { code: 'hide()' });
     showing = false;
   }
   else {
+    chrome.browserAction.setIcon({ path:"active.png" });
     chrome.tabs.executeScript(tab.id, { file: 'grep.js' });
     showing = true;
   }
